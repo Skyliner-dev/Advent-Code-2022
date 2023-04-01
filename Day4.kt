054555134ebc32af25505b25dad3ddf355874197
hotBread <109461607+Skyliner-dev@users.noreply.github.com>
2023-04-01 05:50:26 -0700

    Day 3 finished

---
+++ Day4.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val l = readln().toInt()
+    var c = 0
+    repeat(l) {
+        val n = MutableList(2) { readln() }
+        if (n[0].first() <= n[1].first() && n[0].last() >= n[1].last()) {
+            c++
+        }
+    }
+    println(c)
+}
